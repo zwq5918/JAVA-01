d7aa0ede93c47bb33c26c016c4e9b99930fa6263
Zhangwq <zwq5919@163.com>
2021-01-15 22:14:03 +0800

    第1周作业

---
+++ Week_01/RuleConfig.java
@@ -0,0 +1,17 @@
+package com.bestway.tools;
+
+import java.util.List;
+
+public class RuleConfig {
+	private List<AppRuleConfig> configs;
+
+	public List<AppRuleConfig> getConfigs() {
+		return configs;
+	}
+
+	public void setConfigs(List<AppRuleConfig> configs) {
+		this.configs = configs;
+	}
+	
+
+}
